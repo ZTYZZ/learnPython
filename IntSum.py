f109febec567a978080abb75dadc50f5eff82ede
ZTYZZ <1528723869@qq.com>
2017-11-03 16:26:14 +0800

    happy demo

---
+++ IntSum.py
@@ -0,0 +1,5 @@
+n = input("please input a number")
+sum = 0
+for i in range(int(n)):
+    sum += i+1;
+print("1-N sum is: " ,sum)
---
+++ Sumf.py
@@ -0,0 +1,5 @@
+sum ,tmp = 0,1
+for i in range(1,11):
+    tmp*=i
+    sum +=tmp
+print("the sum is:{}".format(sum))
